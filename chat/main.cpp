0b28ce855b7b7f5ef1d8236ebaf4cbe1e1665bb4
shakeLake <xx002xx015xx@yandex.ru>
2023-01-11 01:14:00 +0500

    libs cmake .sh

---
+++ chat/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "../third-party/asio-1.24.0/include/asio.hpp"
+
+#include "../upnp/upnp.hpp"
+
+int main()
+{
+
+    return 0;
+}
---
+++ upnp/upnp.hpp
@@ -0,0 +1,22 @@
+#ifndef UPNP_HPP_
+#define UPNP_HPP_
+
+#include <iostream>
+
+/* MINIUPNPc */
+#include "../third-party/miniupnpc-2.2.4/include/miniupnpc.h"
+#include "../third-party/miniupnpc-2.2.4/include/miniwget.h"
+#include "../third-party/miniupnpc-2.2.4/include/portlistingparse.h"
+#include "../third-party/miniupnpc-2.2.4/include/upnpcommands.h"
+#include "../third-party/miniupnpc-2.2.4/include/upnperrors.h"
+#include "../build/miniupnpc-build/miniupnpcstrings.h"
+
+class Upnp
+{
+private:
+
+public:
+
+};
+
+#endif /* UPNP_HPP_ */
