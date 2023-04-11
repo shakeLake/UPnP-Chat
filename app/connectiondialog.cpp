aee383443044d644bedab23556c26ec82ebac9a1
Mark <99725123+shakeLake@users.noreply.github.com>
2023-04-11 17:49:36 +0500

    Update connectiondialog.cpp

--- app/connectiondialog.cpp
+++ app/connectiondialog.cpp
@@ -25,16 +25,16 @@ ConnectionDialog::ConnectionDialog()
 	edit_ip->setPlaceholderText(" ip address");
 	edit_ip->setFixedHeight(25);
 	edit_ip->setStyleSheet("background-color: #F1DAC4;"
-						   "border-radius: 5px;"
-						   "color: #161C32;");
+			       "border-radius: 5px;"
+			       "color: #161C32;");
 	style.SetNunitoLineFont(edit_ip);
 
 	edit_port = new QLineEdit();	
 	edit_port->setPlaceholderText(" 49152 - 65535");
 	edit_port->setFixedHeight(25);
 	edit_port->setStyleSheet("background-color: #F1DAC4;"
-							 "border-radius: 5px;"
-							 "color: #161C32");
+				 "border-radius: 5px;"
+				 "color: #161C32");
 	style.SetNunitoLineFont(edit_port);
 
 	cancel_button = new QPushButton("Cancel"); 
@@ -44,8 +44,8 @@ ConnectionDialog::ConnectionDialog()
 	task_button = new QPushButton("Connect");
 	task_button->setFixedSize(125, 24);
 	task_button->setStyleSheet("background-color: #161C32;"
-								"color: white;"
-								"border-radius: 5px");
+				   "color: white;"
+				   "border-radius: 5px");
 
 	// main_label
 	main_layout = new QVBoxLayout(main_widget);	
