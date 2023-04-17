1b807e9692311576af9b0d8ee8b1acfd65351bdb
Mark <99725123+shakeLake@users.noreply.github.com>
2023-04-17 20:56:28 +0500

    Update client.hpp

--- chat/include/client.hpp
+++ chat/include/client.hpp
@@ -14,37 +14,36 @@ namespace ucc
         // endpoint
         asio::ip::tcp::resolver::results_type endpnt;
 			
-		// error code			
+	// error code			
         asio::error_code error;
-			
-	public:
-		// connection status
-		bool connection_status;
+	
+    public:
+	// connection status
+	bool connection_status;
 
-	private:
+    private:
     	/* connects to specific ip address */
-		void Connect();
+	void Connect();
 
-	public:
+    public:
     	Client(asio::io_context& io_c, std::string& ip, std::string& port, ucd::Data* u_d) 
-		: 	rslvr(io_c), 
-			ClientCore(io_c, u_d)
+	:	rslvr(io_c), 
+		ClientCore(io_c, u_d)
         {				
         	endpnt = rslvr.resolve(ip, port, error);
 
-            if (error)
+            	if (error)
            	{
-				u_d->Log(error.message());
-            }
+			u_d->Log(error.message());
+            	}
 
-
-            Connect();
-		}
+            	Connect();
+	}
 
         ~Client()
-		{
-			user_data->Log("Client Core destructor");
-		}
+	{
+		user_data->Log("Client Core destructor");
+	}
     };
 }
 
