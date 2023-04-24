8743f4814ed6b6c9728df682068ec02c123e3d9e
shakeLake <xx002xx015xx@yandex.ru>
2023-04-24 21:12:05 +0500

    -dcmake_path

--- build-release.sh
+++ build-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e "Build"
-cmake -G "MinGW Makefiles" -B "build" -DCMAKE_BUILD_TYPE=Release
+cmake -G "MinGW Makefiles" -B "build" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="C:/Program Files/Qt/lib/cmake"
 
 cd build
 mingw32-make
