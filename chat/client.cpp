630b13f4255cf9d62429ab351ca99b62e33fcc24
Mark <99725123+shakeLake@users.noreply.github.com>
2023-04-24 20:48:03 +0500

    Update client.cpp

--- chat/client.cpp
+++ chat/client.cpp
@@ -2,20 +2,20 @@
 
 void ucc::Client::Connect()
 {
-    asio::connect(sckt, endpnt, error);
- 
-    if (error)
-    {
-    	user_data->Log(error.message());
+	asio::connect(sckt, endpnt, error);
+ 	
+	if (error)
+    	{
+		user_data->Log(error.message());
 		
-	connection_status = false;
-    }
-    else
-    {
-    	user_data->Log("Connected");
+		connection_status = false;	
+	}
+	else
+	{
+		user_data->Log("Connected");
 				
-	connection_status = true;
+		connection_status = true;
 
-	ReceiveFrom(action);
-    }
+		ReceiveFrom(action);
+	}
 }
