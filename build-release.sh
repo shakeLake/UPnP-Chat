e1de8fe6af2ea972285dc91b74e716cf75de9361
shakeLake <xx002xx015xx@yandex.ru>
2023-07-20 20:16:05 +0500

    changes

--- build-release.sh
+++ build-release.sh
@@ -1,20 +1,26 @@
 echo -e "Qt Build"
 
+prefixVar="../qt-dpndncy/lib/cmake"
+
 if [[ ! -d qt-dpndncy ]]; then
 	mkdir qt-dpndncy
 	cd qt-dpndncy
 
-	if [[ "$OSTYPE" == "linux" ]]; then
-		./../third-party/qtbase/configure -static -no-prefix
-		cmake --build . --parallel
+	if [[ "$OSTYPE" == "linux-gnu" ]]; then
+		./../third-party/qtbase/configure -static -prefix "/usr/local/lib/Qt"
+		cmake --build . --parallel 4
 	elif [[ "$OSTYPE" == "msys" ]]; then
 		./../third-party/qtbase/configure -static -platform "win32-g++" -no-prefix
 		cmake --build .
 	fi
 fi
 
+if [[ "$OSTYPE" == "linux-gnu" ]]; then 
+	prefixVar="/usr/local/lib/Qt"
+fi
+
 echo -e "Chat Build"
-cmake -B "build" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="../qt-dpndncy/lib/cmake"
+cmake -B "build" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=prefixVar
 
 cd build
 cmake --build .
